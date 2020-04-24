d22600a699a83cdc92fc430ea5fcedac6fd2d118
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2020-04-24 12:35:10 +0200

    Add router for deep linking workspace creation

--- che-zero/src/App.tsx
+++ che-zero/src/App.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import WorkspaceList from './WorkspaceList';
 import Keycloak from 'keycloak-js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import WorkspaceLauncher from './WorkspaceLauncher';
 
 interface AppProps {
   cheDomain: string;
@@ -10,10 +16,20 @@ interface AppProps {
 
 function App(props: AppProps) {
   return (
-    <div className="App">
-      <h1>[ che-zero ]</h1>
-      <WorkspaceList cheDomain={props.cheDomain} keycloak={props.keycloak} />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <h1>[ che-zero ]</h1>
+            <WorkspaceList cheDomain={props.cheDomain} keycloak={props.keycloak} />
+          </div>
+        </Route>
+        <Route path="/start/:stack/:name">
+          <WorkspaceLauncher cheDomain={props.cheDomain} keycloak={props.keycloak} />
+        </Route>
+      </Switch>
+    </Router>
+
   );
 }
 
