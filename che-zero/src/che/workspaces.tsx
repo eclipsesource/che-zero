3a4f3c165bbe085c7a2490bb1809d65e4ac9ad6c
Lucas Koehler <lkoehler@eclipsesource.com>
2020-07-22 17:52:39 +0200

    Automatically update workspaces status periodically
    Re-fetch the workspaces info every 5 seconds.
    Additional manual fetches do not affect this interval.
    However, parallel fetches are avoided.
    
    * Extend the withWorkspaces HOC to support periodic refresh
    * Add custom useStateWithoutNotifications hook

--- che-zero/src/che/workspaces.tsx
+++ che-zero/src/che/workspaces.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import Keycloak from 'keycloak-js';
-import React, { ComponentType, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { useStateWithoutNotifications } from '../util';
 
 export type WorkspaceStatus = 'RUNNING' | 'STARTING' | 'STOPPED' | 'STOPPING';
 
@@ -41,9 +43,16 @@ export interface WithWorkspacesProps {
 const fetchWorkspaces = (
   cheDomain: string,
   keycloakIdToken: string | undefined,
+  isFetching: () => boolean,
+  setFetching: (fetching: boolean) => void,
   setWorkspaces: React.Dispatch<React.SetStateAction<Workspace[] | undefined>>,
   setError: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
+  if (isFetching()) {
+    return;
+  }
+  setFetching(true);
+
   axios
     .get(`https://che-che.${cheDomain}/api/workspace?skipCount=0&maxItems=30`, {
       headers: {
@@ -57,8 +66,12 @@ const fetchWorkspaces = (
     .catch(() => {
       setError(true);
       setWorkspaces(undefined);
-    });
+    })
+    .finally(() => setFetching(false));
 };
+
+/** Interval to fetch workspaces in milli seconds. */
+const FETCH_INTERVAL = 5000;
 
 /**
  * Injects fetched workspaces in a wrapped component.
@@ -69,14 +82,32 @@ const fetchWorkspaces = (
 export const withWorkspaces = <T extends {}>(
   Component: React.ComponentType<T & WithWorkspacesProps>
 ): React.FC<T & CheProps> => (props) => {
-  const { cheDomain, keycloak } = props;
+  const {
+    cheDomain,
+    keycloak: { idToken },
+  } = props;
   const [workspaces, setWorkspaces] = useState<Workspace[]>();
   const [error, setError] = useState(false);
 
-  const refreshWorkspaces = () =>
-    fetchWorkspaces(cheDomain, keycloak.idToken, setWorkspaces, setError);
+  // Use state that won't trigger dependency change of useCallback below even if a new value is set.
+  const [isFetching, setFetching] = useStateWithoutNotifications(false);
 
-  useEffect(refreshWorkspaces, [cheDomain, keycloak.idToken]);
+  const refreshWorkspaces = useCallback(() => {
+    fetchWorkspaces(
+      cheDomain,
+      idToken,
+      isFetching,
+      setFetching,
+      setWorkspaces,
+      setError
+    );
+  }, [cheDomain, idToken, isFetching, setFetching]);
+
+  useEffect(() => {
+    refreshWorkspaces();
+    const interval = setInterval(refreshWorkspaces, FETCH_INTERVAL);
+    return () => clearInterval(interval);
+  }, [refreshWorkspaces]);
 
   return (
     <Component
